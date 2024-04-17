73e6732257310fc04c28da4e7f79adf47268201d
GuiHenriques <gui13ghc@gmail.com>
2024-04-17 20:43:51 -0300

    commit 1

---
+++ models/funcionario.py
@@ -0,0 +1,23 @@
+from pessoa import Pessoa
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, cpf, email, senha):
+        super().__init__(nome, cpf)
+        self._email = email
+        self._senha = senha
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def senha(self):
+        return self._senha
+    
+    @senha.setter
+    def senha(self, senha):
+        self._senha = senha
---
+++ models/pessoa.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, nome, cpf):
+        self._nome = nome
+        self._cpf = cpf
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @cpf.setter
+    def cpf(self, cpf):
+        self._cpf = cpf
