36ddf1c12b7fe2e9af45dc4bc76b4b2f4bf40092
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2024-04-18 15:26:07 -0300

    achei q era caixa n entrega

---
+++ models/tipo_de_entrega.py
@@ -0,0 +1,43 @@
+
+
+class tipoDeEntrega:
+    def __init__(self, nome, taxa, descricao):
+        self._id = id
+        self._nome = nome
+        self._taxa = taxa
+        self._descricao = descricao
+ 
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def taxa(self):
+        return self._taxa
+
+    @taxa.setter
+    def taxa(self, taxa):
+        self._taxa = taxa
+
+    @property
+    def descricao(self):
+        return self._descricao
+
+    @descricao.setter
+    def descricao(self, descricao):
+        self._descricao = descricao
+
+
