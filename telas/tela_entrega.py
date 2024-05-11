a2b96639dbd31ccf379cbd2633201b202c791c1c
GuiHenriques <gui13ghc@gmail.com>
2024-05-11 12:35:33 -0300

    conflict resolved

--- telas/tela_entrega.py
+++ telas/tela_entrega.py
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-from telas.tela import Tela
-import PySimpleGUI as sg
-
-
-class TelaEntrega(Tela):
-    def __init__(self):
-        super().__init__()
-
-    def tela_entrega(self):
-        layout = [
-            [sg.Text("Entrega", font=("Arial", 24), justification="center")],
-            [
-                sg.Text("CPF do Remetente:", size=(15, 1)),
-                sg.InputText("", key="cpf_remetente", size=(30, 1)),
-            ],
-            [
-                sg.Text("CPF do Destinatário:", size=(15, 1)),
-                sg.InputText("", key="cpf_destinatario", size=(30, 1)),
-            ],
-            [
-                sg.Text("ID da Encomenda:", size=(15, 1)),
-                sg.InputText("", key="id_encomenda", size=(30, 1)),
-            ],
-            [
-                sg.Text(
-                    "Tipo de entrega:",
-                    size=(15, 1),
-                    justification="center",
-                ),
-                sg.Combo(
-                    ["Expressa", "Normal", "Econômica"],
-                    key="tipo_entrega",
-                    size=(28, 1),
-                ),
-            ],
-            [sg.Button("Registrar", size=(8, 1))],
-        ]
-
-        self.janela = sg.Window("Entrega", layout, element_justification="c")
-
-        evento, valores = self.abrir_janela()
-        self.fechar_janela()
-        # {
-        #     "cpf_remetente": "123.456.789-00",
-        #     "cpf_destinatario": "987.654.321-00",
-        #     "id_encomenda": "1",
-        #     "tipo_entrega": "Expressa",
-        # }
-
-        if evento == "Registrar":
-            return evento, valores
-
-        return evento, None
-=======
 import PySimpleGUI as sg
 from telas.telaAbstrata import TelaAbstrata
 
@@ -192,4 +137,3 @@ class TelaEncomenda(TelaAbstrata):
         self.fechar_janela()
 
         return None
->>>>>>> main
