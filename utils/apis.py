eb0548baccf533d57f0838ca96d1d27061acb3c5
GuiHenriques <gui13ghc@gmail.com>
2024-05-26 16:15:47 -0300

    frete

---
+++ utils/apis.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_distancia(cep_destino, mode_idx):
+
+    base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    cep_ufsc = "88040-370"
+
+    modes = ["walking", "bicycling", "transit", "driving"]
+    mode = modes[mode_idx]
+
+    params = {
+        "destinations": cep_destino,
+        "mode": mode,
+    }
+
+    params["origins"] = cep_ufsc
+    params["key"] = os.getenv("API_KEY")
+
+    response = requests.get(base_url, params=params).json()
+
+    if response["status"] == "OK":
+        return response["rows"][0]["elements"][0]
+    else:
+        raise Exception(response["error_message"])
---
+++ utils/frete.py
@@ -0,0 +1,16 @@
+def calcula_valor_total(distancia, peso, dimensoes, taxa_caixa, taxa_entrega):
+    taxa_distancia = 0.05
+    taxa_peso = 2
+    taxa_volume = 5
+
+    distancia = distancia/1000 # convertendo m para km
+
+    volume = dimensoes.altura * dimensoes.largura * dimensoes.comprimento
+
+    valor_total = (distancia * taxa_distancia) + (peso * taxa_peso) + (volume * taxa_volume)
+
+    valor_total *= (1 + taxa_entrega)
+    valor_total += taxa_caixa
+    
+
+    return round(valor_total, 2)
